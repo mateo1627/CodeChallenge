12e448a49120c87c6efc27ca94f4b5a1c7280224
mateo1627 <mateogoni1627@outlook.com>
2023-09-14 19:58:04 -0300

    Estilo y mejora de detalles

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiURL ="https://jsonplaceholder.typicode.com/users";
+/*const apiURL ="https://jsonplaceholder.typicode.com/users";
         const formDeRegistro = document.getElementById('formDeRegistro'); // Obtener el elemento del formulario
 
         function fetchAPIForm() {
@@ -6,6 +6,7 @@ const apiURL ="https://jsonplaceholder.typicode.com/users";
             const objetoForm = {};
             formData.forEach((value, key) => {
                 objetoForm[key] = value;
+                
             });
 
             fetch(apiURL, {
@@ -15,8 +16,48 @@ const apiURL ="https://jsonplaceholder.typicode.com/users";
                     "Content-type": "application/json; charset=UTF-8"
                 }
             })
+           
             .then(response => response.json())
-            .then(json => console.log(json))
+            .then(json => {
+            document.getElementById('valueNombre').value = json.name || '';
+            document.getElementById('valueApellido').value = json.username || '';
+            document.getElementById('valueFecha').value = json.id || '';
+        
+        })
             .catch(error => console.error(error));
         }
-  
+  */
+        document.addEventListener('DOMContentLoaded', function() {
+            const form = document.getElementById('registrationForm');
+            const submitBtn = document.getElementById('submitBtn');
+        
+            submitBtn.addEventListener('click', function() {
+                const nombre = document.getElementById('nombre').value;
+                const apellido = document.getElementById('apellido').value;
+                const fechaNacimiento = document.getElementById('fechaNacimiento').value;
+        
+                const formData = {
+                    nombre,
+                    apellido,
+                    fechaNacimiento
+                };
+        
+                // Realiza la solicitud POST al servidor
+                fetch('https://jsonplaceholder.typicode.com/users', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(formData)
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Respuesta del servidor:', data);
+                    alert('Usuario registrado.');
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    alert('Hubo un error al registrar al usuario');
+                });
+            });
+        });
